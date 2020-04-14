a16431c8bb68ab412e03a26285f6353668f49c23
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 20:58:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,5 @@
 def oxford_comma(array)
   modified_array = array.pop
-  modified_array.push("and " + array.last).join(", ")
+  modified_array.push("and " + array.last)
+  modified_array.join(", ")
 end
