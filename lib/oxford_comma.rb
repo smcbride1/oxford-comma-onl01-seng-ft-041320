6fbc853e859086bb53652cc24e70ada0abe7ba3d
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 21:10:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    last_item = array.last
+    array.pop
+    array.push("and " + last_item).join(" ")
+  elsif array.size > 1
+    last_item = array.last
+    array.pop
+    array.push("and " + last_item).join(", ")
+  else
+    array.join
+  end
 end
